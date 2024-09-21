0ad7e766024d6e40bd65a6f5b5c692f1caebd554
Darshit Patel <darshit12@gmail.com>
2024-09-21 16:27:59 +0530

    Add comments

--- src/main/java/dev/darshit/java_ssh_tunnel/Main.java
+++ src/main/java/dev/darshit/java_ssh_tunnel/Main.java
@@ -9,7 +9,9 @@ public class Main {
     public static void main(String[] args) {
         UserDetails userDetails = new UserDetails("username", "password", "sshHost", 22);
         TunnelDetails tunnelDetails = new TunnelDetails(8090, "remoteHost", 8080);
-        MultiTunneler multiTunneler = new MultiTunneler(userDetails, Set.of(tunnelDetails));
+        // Can create Multiple tunnelDetails and pass it into the Set
+        // Can also read a resources file to create tunnelDetail objects as required
+        MultiTunneler multiTunneler = new MultiTunneler(userDetails, Set.of(tunnelDetails/*, tunnelDetails2, ... */));
         multiTunneler.forward();
     }
 }
